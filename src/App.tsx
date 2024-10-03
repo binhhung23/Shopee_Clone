e3c1562f1b05e4d4a97f82e4a2bd165c5b557b34
BinhHung <binhung2311@gmail.com>
2024-10-03 14:43:57 +0700

    Cấu hình React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+import useRouteElements from 'src/useRouteElements'
+
+function App() {
+  const routeElements = useRouteElements()
+  return <div>{routeElements}</div>
 }
+
+export default App
